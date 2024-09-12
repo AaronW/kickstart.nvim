b2082447e9d27362dee25b97d33518776c3fa1ba
Aaron Weinberger <aw9994@cs.ship.edu>
2024-09-12 14:12:04 -0400

    enable bufferline plugin

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,1 +1,12 @@
-return {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'}
+return {
+    'akinsho/bufferline.nvim', 
+    version = "*", 
+    dependencies = 'nvim-tree/nvim-web-devicons',   
+    config = function()
+        require('bufferline').setup {
+            options = {
+                diagnostics = "nvim_lsp",
+            }
+        }
+    end,
+}
